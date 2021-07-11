5841303a9794e6a7f0a3cb49b512982cad678b37
Christopher Beggs <37783467+clbeggs@users.noreply.github.com>
2021-07-11 12:34:11 -0700

    Update gnome-workspaces.sh

--- gnome-workspaces.sh
+++ gnome-workspaces.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "['<Alt>1']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Alt>2']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Alt>3']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "['<Alt>4']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Alt>5']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Alt>6']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Alt>7']"
@@ -7,6 +13,10 @@ dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "['<Alt>8']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-9 "['<Alt>9']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-10 "['<Alt>0']"
 
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-1 "['<Shift><Alt>exclam']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-2 "['<Shift><Alt>at']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-3 "['<Shift><Alt>numbersign']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-4 "['<Shift><Alt>dollar']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-5 "['<Shift><Alt>percent']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-6 "['<Shift><Alt>asciicircum']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-7 "['<Shift><Alt>ampersand']"
