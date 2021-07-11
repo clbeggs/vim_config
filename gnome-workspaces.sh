b96c894a523bba8547be180cce03706e331b59e7
Christopher Beggs <37783467+clbeggs@users.noreply.github.com>
2021-07-10 18:43:09 -0700

    Create gnome-workspaces.sh

---
+++ gnome-workspaces.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Alt>5']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Alt>6']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Alt>7']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "['<Alt>8']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-9 "['<Alt>9']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-10 "['<Alt>0']"
+
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-5 "['<Shift><Alt>percent']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-6 "['<Primary><Shift>asciicircum']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-6 "['<Primary><Shift>asciicircum']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-7 "['<Shift><Alt>ampersand']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-8 "['<Primary><Shift>asterisk']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-9 "['<Primary><Shift>parenleft']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-10 "['<Primary><Shift>parenright']"
+
+
+dconf write /org/gnome/shell/extensions/pop-shell/focus-down "['<Alt>j']"
+dconf write /org/gnome/shell/extensions/pop-shell/focus-left "['<Alt>h']"
+dconf write /org/gnome/shell/extensions/pop-shell/focus-right "['<Alt>l']"
+dconf write /org/gnome/shell/extensions/pop-shell/focus-up "['<Alt>k']"
+
+
+dconf write /org/gnome/shell/extensions/pop-shell/tile-enter "['<Alt>r']"
+dconf write /org/gnome/desktop/wm/keybindings/tile-move-down "['<Shift><Alt>j']"
+dconf write /org/gnome/desktop/wm/keybindings/tile-move-left "['<Shift><Alt>h']"
+dconf write /org/gnome/desktop/wm/keybindings/tile-move-right "['<Shift><Alt>l']"
+dconf write /org/gnome/desktop/wm/keybindings/tile-move-up "['<Shift><Alt>k']"
+
+
+dconf write /org/gnome/desktop/wm/keybindings/close "['<Shift><Alt>q']"
+
